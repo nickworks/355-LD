769ca5ac31c603068a80973f1fa01222d9cdd46c
Lauren <whitel24@ferris.edu>
2019-10-04 18:09:18 -0400

    Created a new scene and programmed the camera to follow the ball

---
+++ Assets/White/Scripts/FollowTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+
+    void FixedUpdate()
+    {
+        if (target == null) return;
+        transform.position = Vector3.Lerp(transform.position, target.position, .1f);
+    }
+}
