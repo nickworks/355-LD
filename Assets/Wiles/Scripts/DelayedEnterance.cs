ae2a7255af12d398df27e033863211cf433fc85b
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-05 14:30:49 -0400

    You start w/ NOTHING
    +Created DelayedEnterance Script and applied it to all of the yellow objects in my scene. They don't need to be yellow to have the script, yellow just means I made them bouncey.
    *Table now has no obsticles until the player starts playing the game.

---
+++ Assets/Wiles/Scripts/DelayedEnterance.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedEnterance : MonoBehaviour
+{
+
+    public float delayTimer;
+    public Vector3 startingOffset;
+    Rigidbody body;
+    bool iWantToMove = false;
+    bool canIMove = false;
+    Vector3 startingPosition;
+    public enum Trigger { Plunger, LeftFlipper, RightFlipper };
+    public Trigger trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+        Vector3 startMove = transform.position + startingOffset;
+        transform.position = startMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (trigger) {
+            case Trigger.Plunger:
+                if (Input.GetButtonUp("Plunger")) iWantToMove = true;
+                break;
+            case Trigger.LeftFlipper:
+                if (Input.GetButtonUp("FlipperLeft")) iWantToMove = true;
+                break;
+            case Trigger.RightFlipper:
+                if (Input.GetButtonUp("FlipperRight")) iWantToMove = true;
+                break;
+            default:
+                break;
+        }
+        if (iWantToMove) delayTimer  = delayTimer - Time.deltaTime;
+        if (delayTimer <= 0)
+        {
+            canIMove = true;
+            delayTimer = 0;
+        }
+        if (canIMove) transform.position = Vector3.Lerp(transform.position, startingPosition, .01f);
+    }
+}
