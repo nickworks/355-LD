da15ff72d69eb1106ee90fc26bec8bf899f90ffd
Unknown <webbj19@ferris.edu>
2019-10-05 03:32:08 -0400

    changed plundger
    plunger script adds force now

--- Assets/Plundger.cs
+++ Assets/Plundger.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Plundger : MonoBehaviour
 {
     // Start is called before the first frame update
-    void OnCollisionEnter(Collision collision)
-    {
-        ContactPoint[] points = new ContactPoint[collision.contactCount];
-        collision.GetContacts(points);
-        Vector3 force = new Vector3();
-        foreach (ContactPoint cp in points)
-        {
-            force += cp.normal;
-        }
-        force /= -points.Length;
 
-        //Vector3 force = (collision.transform.position - transform.position).normalized;
-        collision.rigidbody.AddForce(force , ForceMode.Impulse);
-        print("anything");
+    void Update()
+    {
+if (Input.GetButtonUp("Plunger"))
+            {
+print("help");
+            Vector3 force = new Vector3(100, 0, 0);
+    }
+    }
+        void OnCollisionStay(Collision collision)
+    {
+        
+            ContactPoint[] points = new ContactPoint[collision.contactCount];
+            collision.GetContacts(points);
+            Vector3 force = new Vector3(0, 0, 0);
+            foreach (ContactPoint cp in points)
+            {
+                force += cp.normal;
+            }
+            force /= -points.Length;
+
+            //Vector3 force = (collision.transform.position - transform.position).normalized;
+            collision.rigidbody.AddForce(force, ForceMode.Force);
+            
+       
     }
 }
