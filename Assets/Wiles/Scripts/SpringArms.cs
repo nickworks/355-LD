00dbe25748df74df5803ed80e8106cbc9e2d70dc
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-04 20:10:09 -0400

    FINALLY GOT THE SPRING ARM SCRIPT TO WORK!
    *SpringArms Script now controls both arms

--- Assets/Wiles/Scripts/SpringArms.cs
+++ Assets/Wiles/Scripts/SpringArms.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class SpringArms : MonoBehaviour
 {
+
+    public GameObject leftArm;
+    public GameObject rightArm;
+
+    HingeJoint targetLeft;
+    HingeJoint targetRight;
+
+    JointSpring targetSpringLeft;
+    JointSpring targetSpringRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetLeft = leftArm.GetComponent<HingeJoint>();
+        targetRight = rightArm.GetComponent<HingeJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //HingeJoint leftSpring = 
+        //HingeJoint rightSpring = 
+
+        //leftSpring.spring = targetLeft.spring;
+        //rightSpring.spring = targetRight.spring;
+
+        targetSpringLeft = targetLeft.spring;
+        targetSpringRight = targetRight.spring;
+
+        targetSpringLeft.targetPosition = 0;
+        targetSpringRight.targetPosition = 0;
+
+        float d = Input.GetAxis("Horizontal");
+        if (d < 0) { // Left is pushed...
+            // Activete Left Arms...
+            print("LEFT!");
+            targetSpringLeft.targetPosition = -90;
+        }
+        if (d > 0){ // Right is pushed...
+            // Activate Right Arms...
+            print("RIGHT!");
+            targetSpringRight.targetPosition = 90;
+        }
+        
+        targetLeft.spring = targetSpringLeft;
+        targetRight.spring = targetSpringRight;
+
+        //leftArm = targetLeft;
+        //rightArm = targetRight;
         
     }
 }
