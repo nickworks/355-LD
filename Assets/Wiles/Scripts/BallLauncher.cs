9958dfe64ffc70a820b0532b91e3dfb43a3b7519
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-05 02:23:28 -0400

    Disabled the mechanical Plunger and now use direct force on Ball
    -disabled the PlungerControl Script on the Plunger gameObject.
    +Added BallLauncher Script and IT WORKS!

---
+++ Assets/Wiles/Scripts/BallLauncher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+    private bool inPlay;
+    public float force = 0;
+    Rigidbody body;
+    public float maxForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inPlay = false;
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay)
+        {
+            if (Input.GetButton("Plunger"))
+            {
+                force = force + Time.deltaTime;
+                if (force > maxForce) force = maxForce;
+            }
+            if (Input.GetButtonUp("Plunger"))
+            {
+                body.AddForce(new Vector3(0, 0, force), ForceMode.Impulse);
+                inPlay = true;
+            }
+        }
+    }
+}
--- Assets/Wiles/Scripts/PlungerControls.cs
+++ Assets/Wiles/Scripts/PlungerControls.cs
@@ -19,7 +19,7 @@ public class PlungerControls : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         switch(currentState){
