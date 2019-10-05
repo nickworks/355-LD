a7c47b552c63a1b5a7d44f9c2eb7d79a9a64772f
Unknown <webbj19@ferris.edu>
2019-10-05 02:52:29 -0400

    added plunger
    added code for plunger
    Added cube for top of pinball

---
+++ Assets/Plundger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plundger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] points = new ContactPoint[collision.contactCount];
+        collision.GetContacts(points);
+        Vector3 force = new Vector3();
+        foreach (ContactPoint cp in points)
+        {
+            force += cp.normal;
+        }
+        force /= -points.Length;
+
+        //Vector3 force = (collision.transform.position - transform.position).normalized;
+        collision.rigidbody.AddForce(force , ForceMode.Impulse);
+        print("anything");
+    }
+}
