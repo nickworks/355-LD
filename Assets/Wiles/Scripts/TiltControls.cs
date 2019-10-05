012b9682b2de655d3abc35e442c7f116ece1f2b7
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-05 16:33:02 -0400

    Tilt Controls Completed. .. Sort of.
    *Tilt logic works now.

--- Assets/Wiles/Scripts/TiltControls.cs
+++ Assets/Wiles/Scripts/TiltControls.cs
@@ -16,15 +16,20 @@ public class TiltControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-            //print("Horizontal is down!");
-            /*
-            float tilt = -(Input.GetAxisRaw("Horizontal"));
-            Transform tiltTrans = transform;
-            tiltTrans.eulerAngles = Vector3.Lerp(tiltTrans.eulerAngles, new Vector3(tiltTrans.eulerAngles.x, tiltTrans.eulerAngles.y, maxTilt * tilt), .65f);
-        print(tiltTrans.eulerAngles);
-            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, tiltTrans.eulerAngles, .65f);
-            */
+        Quaternion maxQuart = Quaternion.Euler(-6.5f, 0, maxTilt);
+        Quaternion minQuart = Quaternion.Euler(-6.5f, 0, minTilt);
+        Quaternion defQuart = Quaternion.Euler(-6.5f, 0, 0);
+        if (Input.GetAxisRaw("Horizontal") == -1) transform.localRotation = maxQuart;
+        if (Input.GetAxisRaw("Horizontal") == 1) transform.localRotation = minQuart;
+        if (Input.GetAxisRaw("Horizontal") == 0) transform.rotation = defQuart;
+        //print("Horizontal is down!");
+        /*
+        float tilt = -(Input.GetAxisRaw("Horizontal"));
+        Transform tiltTrans = transform;
+        tiltTrans.eulerAngles = Vector3.Lerp(tiltTrans.eulerAngles, new Vector3(tiltTrans.eulerAngles.x, tiltTrans.eulerAngles.y, maxTilt * tilt), .65f);
+    print(tiltTrans.eulerAngles);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, tiltTrans.eulerAngles, .65f);
+        */
 
         /*
         float h = Input.GetAxisRaw("Horizontal");
