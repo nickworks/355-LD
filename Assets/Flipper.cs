8c80420c23d49b3c04e230efbd2418edca0bb2e2
Unknown <webbj19@ferris.edu>
2019-10-05 00:16:25 -0400

    made pinball
    added hinges to flippers
    added script to move flippers
    added slot for pinball

---
+++ Assets/Flipper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    Rigidbody body;
+    HingeJoint joint;
+    public float capultSrentgh = 300;
+
+    public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        joint = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetButtonDown("FlipperLeft"))
+        {
+            JointSpring spring = new JointSpring();
+            spring.targetPosition = 65;
+            spring.spring = capultSrentgh;
+            joint.spring = spring;
+            joint.useSpring = true;
+           
+
+
+        }
+        if (Input.GetButtonUp("FlipperLeft"))
+        {
+            JointSpring spring = new JointSpring();
+            spring.targetPosition = -15;
+            spring.spring = capultSrentgh;
+            joint.spring = spring;
+            joint.useSpring = true;
+
+
+
+        }
+    }
+
+}
