3ad1a8143de8b455b3843f33e2e9d0335ab96a00
Jacob Wiles <Awsoemman328@gmail.com>
2019-10-10 15:37:43 -0400

    Added Plunger Power Bar
    +Added Canvas w/ an image being used as a power progress bar added right above plunger
    +imported pixel texture sprite for above bar

---
+++ Assets/Wiles/Scripts/PlungerBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlungerBar : MonoBehaviour
+{
+
+    public Image progressBar;
+    public Text progressText;
+
+    float power;
+    float powerMax;
+
+    public GameObject pinballReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        power = pinballReference.GetComponent<BallLauncher>().force;
+        powerMax = pinballReference.GetComponent<BallLauncher>().maxForce;
+
+        progressBar.fillAmount = power / powerMax;
+        progressText.text = $"{progressBar.fillAmount * 100}%";
+    }
+}
