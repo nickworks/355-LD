79e359cbeafa4c0f405b06e94a5d5ca23b739752
Unknown <webbj19@ferris.edu>
2019-10-10 15:17:38 -0400

    made demo GUI
    made demo GUI

---
+++ Assets/Webb/Scripts/HUDControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image progressBar;
+    float xp = 0;
+    float xpMax = 350;
+    public Text textXp;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        xp += 50 * Time.deltaTime;
+        float p = xp / xpMax;
+        progressBar.fillAmount = xp / xpMax;
+    textXp.text = $"XP:{(int)xp}";
+    }
+    public void ClearXpValue()
+    {
+        xp = 0;
+    }
+}
