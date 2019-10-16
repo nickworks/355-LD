6bbfe60a6cbf9111dcb5fd0584ad5dae09fa8d0a
Unknown <whitel24@ferris.edu>
2019-10-16 18:32:32 -0400

    The ball now gets destroyed once it has rolled past the flippers

---
+++ Assets/White/Scripts/KillBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace White
+{
+    public class KillBall : MonoBehaviour
+    {
+        public float killPlane;
+        public GameObject ball;
+        
+        void Update()
+        {
+            if (ball.transform.position.z < killPlane)
+            {
+                Destroy(ball);
+            }
+        }
+    }
+}
